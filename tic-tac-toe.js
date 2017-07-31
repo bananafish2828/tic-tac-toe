49ac0daccefe0890e8cd646effe62592846d374b
bananafish2828 <bananafish2828@gmail.com>
2017-07-31 16:49:55 -0700

    (add) initialize board

---
+++ tic-tac-toe.js
@@ -0,0 +1,27 @@
+let board = {
+  1: ' ',
+  2: ' ',
+  3: ' ',
+  4: ' ',
+  5: ' ',
+  6: ' ',
+  7: ' ',
+  8: ' ',
+  9: ' ',
+
+  showBoard: () => {
+    console.log(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3]);
+    console.log('-----------');
+    console.log(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6]);
+    console.log('-----------');
+    console.log(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9]);
+  }
+
+
+
+
+
+
+}
+
+board.showBoard();
